05cb76bc88986261f88c5f1db15788323de1af7b
Ricardo Solis <rds11@sfu.ca>
2024-06-13 16:50:21 -0700

    add authentication

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,9 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 import {
-  Grid,
-  Typography,
-  Box
+    Grid,
+    Typography,
+    Box, AppBar, Toolbar, Button
 } from "@mui/material";
 
 import AchievementList from "./AchievementList.tsx";
@@ -14,31 +14,136 @@ import FilterBar from "./FilterBar.tsx";
 import GamesList from "./GamesList.tsx";
 import {Game} from "../interfaces/types.tsx";
 import TitleBar from "./TitleBar.tsx";
+import axios from "axios";
 
 function App() {
 
-  //Define state variables
-  const [sortFilter, setSortFilter] = useState<number>(0)
-  const [visibleFilter, setVisibleFilter] = useState<boolean[]>([false, false]);
-  const [selectedGame, setSelectedGame] = useState<Game>();
+    //Define state variables
+    const [sortFilter, setSortFilter] = useState<number>(0)
+    const [visibleFilter, setVisibleFilter] = useState<boolean[]>([false, false]);
+    const [selectedGame, setSelectedGame] = useState<Game>();
+    const [isAuthenticated, setIsAuthenticated] = useState(false); // null indicates loading state
+    const [userData, setUserData] = useState(null);
 
+    //Helper function used to get user info from server
+    const fetchUserData = async () => {
+        try {
+            console.log("Fetching user data!")
+            const response = await axios.get('http://localhost:3000/auth/steam/user',{
+                withCredentials: true,
+            });
 
-  //Component Handlers
+            setUserData(response.data);
+            console.log(response.data)
+        } catch (error) {
+            console.error('Error fetching user data:', error);
+        }
+    };
 
-  //GamesList Handlers
-  const updateSelectedGameState = (game : Game) =>{
+    //Checks If user is logged in upon page load
+    useEffect(() => {
+        const checkAuthentication = async () => {
+            try {
+
+                const response = await axios.get('http://localhost:3000/auth/steam/checkAuthenticated', {
+                    withCredentials: true, // Important to include credentials
+                });
+
+                if (!response.data.authenticated) {
+                    //window.location.href = 'http://localhost:3000/auth/steam/login'; // Redirect to backend route for Steam login
+                    setIsAuthenticated(false);
+                    console.log("First Load Failure: ", userData)
+                } else {
+                    setIsAuthenticated(true);
+                    await fetchUserData();
+                    console.log("First Load  success: ", userData)
+                }
+            } catch (error) {
+                console.error('Error checking authentication:', error);
+                setIsAuthenticated(false);
+            }
+        };
+        checkAuthentication();
+        /*
+        //Delay 3 seconds to give the user time to read redirect message
+        const delayCheck = setTimeout(() => {
+        }, 3000);
+        // Cleanup the timeout if the component is unmounted
+        return () => clearTimeout(delayCheck);
+        */
+    }, []);
+
+    //Component Handlers
+
+    //GamesList Handlers
+    const updateSelectedGameState = (game : Game) =>{
     setSelectedGame(game)
-  }
+    }
 
-  //FilterBar Handlers
-  const updateSortFilterState = (n : number) => {
+    //FilterBar Handlers
+    const updateSortFilterState = (n : number) => {
     if(n != -1){
       setSortFilter(n);
     }
-  };
-  const updateVisibleFilterState = (index : number) => {
+    };
+    const updateVisibleFilterState = (index : number) => {
     setVisibleFilter(prevState => prevState.map((item, idx) => idx === index ? !item : item))
-  };
+    };
+
+    //Handle when the user hits login button
+    const handleLogin = async () => {
+
+        const checkAuthentication = async () => {
+            try {
+                if (!isAuthenticated) {
+                    window.location.href = 'http://localhost:3000/auth/steam/login'; // Redirect to backend route for Steam login
+
+                    //This forces a return back to the main page, calling useEffect
+                    await axios.get('http://localhost:3000/auth/steam/checkAuthenticated', {
+                        withCredentials: true, // Important to include credentials
+                    });
+
+                }
+            } catch (error) {
+                console.error('Error checking authentication:', error);
+                setIsAuthenticated(false);
+            }
+        };
+        checkAuthentication()
+    }
+
+    //handle when the user hits the logout button
+    const handleLogout = async () => {
+        window.location.href = 'http://localhost:3000/auth/steam/logout';
+
+        const response = await axios.get('http://localhost:3000/auth/steam/checkAuthenticated/', {
+            withCredentials: true, // Important to include credentials
+        });
+
+        if (!response.data.authenticated) {
+            setIsAuthenticated(false)
+        } else {
+            setIsAuthenticated(true)
+            console.log("Failed to Logout");
+        }
+    }
+
+    //Render different screen if not logged in
+    if (!isAuthenticated) {
+        return (
+            <>
+                <Grid container spacing={0.5} >
+                    <AppBar position="static" style={{ marginBottom: '10px' }}>
+                        <Toolbar>
+                            <Button variant = "contained" onClick = {() => {handleLogin()}}>Login</Button>
+                            <Typography>Upon returning to this page, please wait a few seconds</Typography>
+                        </Toolbar>
+                    </AppBar>
+                </Grid>
+            </>
+        )
+    }
+
 
   /* Helper Functions */
 
@@ -46,7 +151,13 @@ function App() {
     <>
 
       {/* Main Body Content */}
-      <Grid container spacing={0.5} style={{ marginTop: '20px' }}>
+      <Grid container spacing={0.5} >
+
+          <AppBar position="static" style={{ marginBottom: '10px' }}>
+              <Toolbar>
+                  <Button variant = "contained" onClick = { () => {handleLogout()} }>Logout</Button>
+              </Toolbar>
+          </AppBar>
 
         {/*Games Bar*/}
         <GamesList
